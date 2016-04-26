ac27bd30932c61fe43bf2e173ec8e43847391061
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2016-04-26 13:54:00 +0900

    phpcs 用引数処理は phpcs スクリプトファイルで行う

--- src/phpcs-git-blob.sh
+++ src/phpcs-git-blob.sh
@@ -3,19 +3,15 @@
 # インデックスされたファイル
 blob_index=${1:?"index of first argument missing"}
 
-# phpcs 規則定義
-standard_file=$2
-
-# ファイルのパス
-file_path=${3:+--stdin-path="$3"}
+# 第 2 以降の引数はそのまま phpcs.sh に渡す
+shift
 
 # 実行バイナリ
 git_bin="${GIT_BIN:-git}"
 
-# 規則定義が指定されている時のパラメーター
-phpcs_standard_param="${standard_file:+--standard=$standard_file}"
+# phpcs 実行スクリプトの相対パス
 phpcs_script="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )/phpcs.sh"
 
 # 実行
 $git_bin cat-file blob "$blob_index" | \
-  bash "$phpcs_script" $phpcs_standard_param $file_path
+  bash "$phpcs_script" $@
--- src/phpcs.sh
+++ src/phpcs.sh
@@ -1,11 +1,19 @@
 # phpcs を実行するスクリプト
 
+# 第 1 引数は phpcs 規則定義
+phpcs_standard_param="${1:+--standard=$1}"
+shift
+
+# 第 2 引数はファイルのパス
+file_path=${1:+--stdin-path="$1"}
+shift
+
 # 実行バイナリ
 phpcs_bin="${PHPCS_BIN:-phpcs}"
 
 "$phpcs_bin" \
   --report=emacs --no-colors --encoding=utf-8 \
-  $@
+  $phpcs_standard_param $file_path $@
 
 # phpcs が実行できたので正常終了
 exit 0
