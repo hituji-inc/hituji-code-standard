4e2ccb59077b3352a2a16d9b2d4f1c08cfa61526
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2016-04-26 13:37:03 +0900

    バイナリー存在チェックはスクリプトの最初で行う

--- src/phpcs.sh
+++ src/phpcs.sh
@@ -3,12 +3,6 @@
 # 実行バイナリ
 phpcs_bin="${PHPCS_BIN:-phpcs}"
 
-# 見つからないときはエラーで終了
-if ! hash "$phpcs_bin" 2>/dev/null; then
-  echo '$PHPCS_BIN must be specified.' "$phpcs_bin": not found 1>&2
-  exit 1;
-fi
-
 "$phpcs_bin" \
   --report=emacs --no-colors --encoding=utf-8 \
   $@
