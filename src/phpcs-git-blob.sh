0fd99074f9444cd3dc86ec733ba828ca732b4317
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2016-04-26 13:05:07 +0900

    第3引数でファイルパスを受け付けられるように
    phpcs は 2.6 以上でなくてはならない

--- src/phpcs-git-blob.sh
+++ src/phpcs-git-blob.sh
@@ -6,6 +6,9 @@ blob_index=${1:?"index of first argument missing"}
 # phpcs 規則定義
 standard_file=$2
 
+# ファイルのパス
+file_path=${3:+--stdin-path="$3"}
+
 # 実行バイナリ
 git_bin="${GIT_BIN:-git}"
 
@@ -15,4 +18,4 @@ phpcs_script="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )/phpcs.sh"
 
 # 実行
 $git_bin cat-file blob "$blob_index" | \
-  bash "$phpcs_script" $phpcs_standard_param
+  bash "$phpcs_script" $phpcs_standard_param $file_path
